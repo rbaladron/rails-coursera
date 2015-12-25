344b2656aab9d056d21601c6d08b005719ca7fac
rbaladron <rbaladron@gmail.com>
2015-12-25 17:34:57 +0100

    Default scopes y named scopes en class Person

--- active_record/Module2/advanced_ar/db/migrate/20151225140501_create_hobbies_people.rb
+++ active_record/Module2/advanced_ar/db/migrate/20151225140501_create_hobbies_people.rb
@@ -1,6 +1,6 @@
 class CreateHobbiesPeople < ActiveRecord::Migration
   def change
-    create_table :hobbies_people do |t|
+    create_table :hobbies_people, id: false do |t|
       t.references :person, index: true, foreign_key: true
       t.references :hobby, index: true, foreign_key: true
     end
