013de2c5608300a4c8faee16446249276c5b2d16
rbaladron <rbaladron@gmail.com>
2015-12-25 15:08:05 +0100

    Creación de modelo hobby y sus relaciones con person

--- active_record/Module2/advanced_ar/app/models/person.rb
+++ active_record/Module2/advanced_ar/app/models/person.rb
@@ -2,4 +2,5 @@ class Person < ActiveRecord::Base
   has_one :personal_info, dependent: :destroy
   has_many :jobs
   has_many :my_jobs, class_name: "Job"
+  has_and_belongs_to_many :hobbies
 end
--- active_record/Module2/advanced_ar/db/schema.rb
+++ active_record/Module2/advanced_ar/db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151225125401) do
+ActiveRecord::Schema.define(version: 20151225140501) do
+
+  create_table "hobbies", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "hobbies_people", force: :cascade do |t|
+    t.integer "person_id"
+    t.integer "hobby_id"
+  end
+
+  add_index "hobbies_people", ["hobby_id"], name: "index_hobbies_people_on_hobby_id"
+  add_index "hobbies_people", ["person_id"], name: "index_hobbies_people_on_person_id"
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
