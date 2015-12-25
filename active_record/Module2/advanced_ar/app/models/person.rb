93cf1a637568e4b7b955ef070454da13056829a8
rbaladron <rbaladron@gmail.com>
2015-12-25 15:21:27 +0100

    Creación de modelo salary_range y su relaciones.

--- active_record/Module2/advanced_ar/app/models/person.rb
+++ active_record/Module2/advanced_ar/app/models/person.rb
@@ -3,4 +3,9 @@ class Person < ActiveRecord::Base
   has_many :jobs
   has_many :my_jobs, class_name: "Job"
   has_and_belongs_to_many :hobbies
+  has_many :approx_salaries, through: :jobs, source: :salary_range
+
+  def max_salary
+    approx_salaries.maximum(:max_salary)
+  end
 end
