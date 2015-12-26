f61ba70723b13240e1be9b72af07341f9c72320c
rbaladron <rbaladron@gmail.com>
2015-12-26 11:38:15 +0100

    pasados todos los test.

--- active_record/Module2/assignment01-ActiveRecord-Relations/todolist/db/migrate/20151225183943_add_user_ref_to_todolists.rb
+++
@@ -1,5 +0,0 @@
-class AddUserRefToTodolists < ActiveRecord::Migration
-  def change
-    add_reference :todo_lists, :user, index: true, foreign_key: true
-  end
-end
--- active_record/Module2/assignment01-ActiveRecord-Relations/todolist/db/migrate/20151225183946_add_user_ref_to_profile.rb
+++
@@ -1,5 +0,0 @@
-class AddUserRefToProfile < ActiveRecord::Migration
-  def change
-    add_reference :profiles, :user, index: true, foreign_key: true
-  end
-end
