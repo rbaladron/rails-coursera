171f4dc2ce755a415ecfbce6306771e97d873288
rbaladron <rbaladron@gmail.com>
2015-12-27 15:40:45 +0100

    Comienzo ejercicio módulo 3

---
+++ intro_rails/modulo3/Assignments/student-start/module3_1_assignment.rb
@@ -0,0 +1,13 @@
+require 'httparty'
+
+class Recipe
+  include HTTParty
+
+  base_uri 'http://food2for.com/api'
+  default_params key:ENV["FOOD2FORK_KEY"]
+  format :json
+
+  def self.for (keyword)
+    get("/search", query: {q: keyword})["recipes"]
+  end
+end
