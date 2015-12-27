ea39792ce28c9a7bda89c0954e7726cd45e39d9d
rbaladron <rbaladron@gmail.com>
2015-12-27 12:31:14 +0100

    Moving Business Logic Out

--- intro_rails/modulo3/my_first_app/app/controllers/greeter_controller.rb
+++ intro_rails/modulo3/my_first_app/app/controllers/greeter_controller.rb
@@ -1,5 +1,10 @@
 class GreeterController < ApplicationController
   def hello
+    @random_names =  ["Paco", "Javier", "Mateo"]
+    @name = random_names.sample
+    @time = Time.now
+    @times_displayed ||= 0
+    @times_displayed += 1
   end
   def goodbye
   end
