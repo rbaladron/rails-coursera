8793ba05bbc51bb829d9c54e708cb2ce4470a7e9
rbaladron <rbaladron@gmail.com>
2015-12-27 15:41:14 +0100

    Actualizado método get_completed_count y nuevo assignment_spec.rb

--- active_record/Module2/assignment01-ActiveRecord-Relations/todolists/app/models/user.rb
+++ active_record/Module2/assignment01-ActiveRecord-Relations/todolists/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_many :todo_items, through: :todo_lists, source: :todo_items
   validates :username, presence: true
 
-  def get_completed_count u
-    u.todo_items.where(completed: true).count
+  def get_completed_count
+    todo_items.where(completed: true).count
   end
 end
