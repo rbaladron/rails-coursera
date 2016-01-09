c2efe88409feb52e716c76cbef7a6f7ae2aef1a2
rbaladron <rbaladron@gmail.com>
2016-01-09 16:24:03 +0100

    Creadas relaciones entre los modelos

--- active_record/Module4/i_reviewed/app/models/reviewer.rb
+++ active_record/Module4/i_reviewed/app/models/reviewer.rb
@@ -1,2 +1,3 @@
 class Reviewer < ActiveRecord::Base
+  has_many :books
 end
--- active_record/Module4/i_reviewed/db/seeds.rb
+++ active_record/Module4/i_reviewed/db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Book.destroy_all
+
+Book.create! [
+  { name: "Eloquent Ruby", author: "Russ Olsen"},
+  { name: "Beginning Ruby", author: "Peter Cooper"},
+  { name: "Metaprogramming Ruby 2", author: "Paolo Perrota"},
+  { name: "Design Patterns in Ruby", author: "Russ Olsen"},
+  { name: "The Ruby Programming Language", author: " David Flanagan"}
+]
