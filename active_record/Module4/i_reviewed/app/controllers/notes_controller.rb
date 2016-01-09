890209b68870e6e7f02814173991f6a653f670fa
rbaladron <rbaladron@gmail.com>
2016-01-09 16:52:45 +0100

    Métodos del Controlador de Notes. Strong parameters

--- active_record/Module4/i_reviewed/app/controllers/notes_controller.rb
+++ active_record/Module4/i_reviewed/app/controllers/notes_controller.rb
@@ -1,2 +1,27 @@
 class NotesController < ApplicationController
+  before_action :set_book, only: [:create, :destroy]
+
+  def create
+    @note = @book.netos.new(note_params)
+    if @note.save
+      redirecto_to @book, notice: "Note succesfully added!"
+    else
+      redirecto_to @book, alert: "Unnable to add note!"
+    end
+  end
+
+  def destroy
+    @note = @book.notes.find(params[:id])
+    @note.destroy
+    redirecto_to @book, notice: "Note deleted"
+  end
+
+  private
+    def set book
+      @book = Book.find(params[:book_id])
+    end
+
+    def note_params
+      params.require(:note).permi(:title, :note)
+    end
 end
