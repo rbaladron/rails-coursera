e855f828273c24ce62bea53e2ab2424218c02f3a
rbaladron <rbaladron@gmail.com>
2016-01-20 12:00:40 +0100

    Find method

--- web-services-mongodb/Modulo1/Lesson03-Integrating-MongoDB-and-Rails/zips/app/models/concerns/zip.rb
+++ web-services-mongodb/Modulo1/Lesson03-Integrating-MongoDB-and-Rails/zips/app/models/concerns/zip.rb
@@ -10,3 +10,21 @@ class Zip
     self.mongo_client['zips']
   end
 end
+
+# implement a find that returns a collection of document as hashes.
+# Use initializae(hash) to express individual documents as a class
+# instance.
+#     * prototype - quey example for value equality
+#     * sort - hash expressing multi-term sort order
+#     * offset - document to start results
+#     * limit - number of documents to include
+def self.all(prototype{}, sort={:population=>1}, offset=0, limit=100)
+  # map internal :population term to :pop document term
+  tmp = {} # hash needs to stay in stable order provided
+  sort.each {|k,v
+    k = k.to_sym==:population ? :pop : k.to_sym
+    tmp[k] = v if [:city, :state, :pop].include?(k)
+  }
+  sort=tmp
+  # convert to keys and then eliminate any properties no of interest
+  prototype.each_with_object({}) {|(k,v), tmp | tmp[k.to_sym] = v; tmp}
