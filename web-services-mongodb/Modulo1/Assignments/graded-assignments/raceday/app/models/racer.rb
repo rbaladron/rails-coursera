b8b70d8c48027109f58a26d1671a4de34c8fda13
rbaladron <rbaladron@gmail.com>
2016-01-21 16:42:29 +0100

    Pasada especificación rq01

---
+++ web-services-mongodb/Modulo1/Assignments/graded-assignments/raceday/app/models/racer.rb
@@ -0,0 +1,13 @@
+class Racer
+  include ActiveModel::Model
+
+  # convenience method for access to client in console
+  def self.mongo_client
+   Mongoid::Clients.default
+  end
+
+  # convenience method for access to racers collection
+  def self.collection
+   self.mongo_client['racers']
+  end
+end
