585b0637bf59f34a72c1f69a1bdc325612633e9d
rbaladron <rbaladron@gmail.com>
2016-02-05 20:29:58 +0100

    Pasada especificación race 01

---
+++ web-services-mongodb/Modulo3/Assignments/graded-assignments/assignment-Mongoid-and-Rails-Scaffold/triresults/app/models/race.rb
@@ -0,0 +1,9 @@
+class Race
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :n, as: :name, type: String
+  field :date, as: :date, type: Date
+  field :loc, as: :location, type: Address
+
+end
