69f1993a3f6a5bdcd86a1f383741a0b1e9918b60
rbaladron <rbaladron@gmail.com>
2016-02-05 21:11:31 +0100

    Pasada especificación race 05

--- web-services-mongodb/Modulo3/Assignments/graded-assignments/assignment-Mongoid-and-Rails-Scaffold/triresults/app/models/race.rb
+++ web-services-mongodb/Modulo3/Assignments/graded-assignments/assignment-Mongoid-and-Rails-Scaffold/triresults/app/models/race.rb
@@ -5,6 +5,8 @@ class Race
   field :n, as: :name, type: String
   field :date, as: :date, type: Date
   field :loc, as: :location, type: Address
+  scope :upcoming, -> { where(:date.gte => Date.current)}
+  scope :past, -> { where(:date.lt => Date.current)}
 
   embeds_many :events, class_name: 'Event', order: [:order.asc], as: :parent
 
