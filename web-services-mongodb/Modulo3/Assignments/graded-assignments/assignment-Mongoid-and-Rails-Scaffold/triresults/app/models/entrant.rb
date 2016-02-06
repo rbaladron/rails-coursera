14871576831bd283b44747fe56a6f85c384f9cf8
rbaladron <rbaladron@gmail.com>
2016-02-06 11:14:50 +0100

    Pasada especificación flatten_attribute 04

--- web-services-mongodb/Modulo3/Assignments/graded-assignments/assignment-Mongoid-and-Rails-Scaffold/triresults/app/models/entrant.rb
+++ web-services-mongodb/Modulo3/Assignments/graded-assignments/assignment-Mongoid-and-Rails-Scaffold/triresults/app/models/entrant.rb
@@ -42,10 +42,45 @@ class Entrant
   end
 
   def update_total(result)
-    if self.secs.nil?
-      self.secs = result.secs
-    else
-      self.secs += result.secs
+    self.secs = 0
+    self.results.each do |result|
+      if result.secs
+        self.secs = self.secs + result.secs
+      end
+    end
+  end
+
+  RESULTS = {"swim"=>SwimResult,
+    "t1"=>LegResult,
+    "bike"=>BikeResult,
+    "t2"=>LegResult,
+    "run"=>RunResult}
+
+  RESULTS.keys.each do |name|
+    #create_or_find result
+    define_method("#{name}") do
+      result=results.select {|result| name==result.event.name if result.event}.first
+      if !result
+        result=RESULTS["#{name}"].new(:event=>{:name=>name})
+        results << result
+      end
+      result
+    end
+
+    #assign event details to result
+    define_method("#{name}=") do |event|
+      event=self.send("#{name}").build_event(event.attributes)
+    end
+
+    #expose setter/getter for each property of each result
+    RESULTS["#{name}"].attribute_names.reject {|r|/^_/===r}.each do |prop|
+      define_method("#{name}_#{prop}") do
+        event=self.send(name).send(prop)
+      end
+      define_method("#{name}_#{prop}=") do |value|
+        event=self.send(name).send("#{prop}=",value)
+        update_total nil if /secs/===prop
+      end
     end
   end
 
