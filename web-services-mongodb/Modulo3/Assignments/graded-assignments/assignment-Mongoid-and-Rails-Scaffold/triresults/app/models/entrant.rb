1bcc3b3afed64eadd487ba98b5de262d7162e9c4
rbaladron <rbaladron@gmail.com>
2016-02-06 11:08:34 +0100

    Pasada especificación flatten_attribute 03

--- web-services-mongodb/Modulo3/Assignments/graded-assignments/assignment-Mongoid-and-Rails-Scaffold/triresults/app/models/entrant.rb
+++ web-services-mongodb/Modulo3/Assignments/graded-assignments/assignment-Mongoid-and-Rails-Scaffold/triresults/app/models/entrant.rb
@@ -10,8 +10,32 @@ class Entrant
   field :group, as: :group, type: Placing
 
   embeds_many :results, class_name: 'LegResult', order: [:"event.o".asc], after_add: :update_total
-  embeds_one :race, class_name: 'RaceRef'
-  embeds_one :racer, class_name: 'RacerInfo', as: :parent
+  embeds_one :race, class_name: 'RaceRef', autobuild: true
+  embeds_one :racer, class_name: 'RacerInfo', as: :parent, autobuild: true
+
+  delegate :first_name, :first_name=, to: :racer
+  delegate :last_name, :last_name=, to: :racer
+  delegate :gender, :gender=, to: :racer, prefix: "racer"
+  delegate :birth_year, :birth_year=, to: :racer
+  delegate :city, :city=, to: :racer
+  delegate :state, :state=, to: :racer
+
+  delegate :name, :name=, to: :race, prefix: "race"
+  delegate :date, :date=, to: :race, prefix: "race"
+
+
+  def overall_place
+    overall.place if overall
+  end
+  def gender_place
+    gender.place if gender
+  end
+  def group_name
+    group.name if group
+  end
+  def group_place
+    group.place if group
+  end
 
   def the_race
     self.race.race
