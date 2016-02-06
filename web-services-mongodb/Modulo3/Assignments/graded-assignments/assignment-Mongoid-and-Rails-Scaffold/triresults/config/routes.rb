8044510aa2fa14f632572a80f766ab16a9ab624d
rbaladron <rbaladron@gmail.com>
2016-02-06 11:55:07 +0100

    Pasada especificación web_resources 01

--- web-services-mongodb/Modulo3/Assignments/graded-assignments/assignment-Mongoid-and-Rails-Scaffold/triresults/config/routes.rb
+++ web-services-mongodb/Modulo3/Assignments/graded-assignments/assignment-Mongoid-and-Rails-Scaffold/triresults/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  resources :racers do
+    post "entries" => "racers#create_entry"
+  end
+  resources :races
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
